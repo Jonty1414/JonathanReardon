58352fe9d0e3efd7537794eab70693a46ca58536
Jonty1414 <Jonty1414@gmail.com>
2020-01-09 18:01:15 +1300

    Add files via upload

---
+++ wwwroot/AimTrainerGame/Button.js
@@ -0,0 +1,53 @@
+﻿/*
+ * Author: Jonathan Reardon.
+ * Project: AimTrainerGame.
+ *
+ * Summary: this project is heavily inspire by a flash based game "http://www.aimbooster.com/", all visual aspects are being created using the p5.js library "https://p5js.org/".
+ */
+
+
+/* button class
+ * these object will store and show the text and buttons onto the screen
+ */
+class Button {
+    //constructor requires 4 arguments (string text, int thieSize, boolean box, int x, int y) 
+    constructor(text, theSize, box, x, y) {
+
+        this.text = text;                           //string; the text to be displayed.
+
+        this.box = box;                             //boolean; if the text has abox around it, true/false.
+
+        this.x = x;                                 //int; x location within canvas.
+
+        this.y = y;                                 //int; y location within canvas.
+
+        this.boxWidth = 75;                         //int; box width.
+
+        this.boxHeight = 50;                        //int; box height.
+
+        this.theSize = theSize;                     //int; text font size.
+
+        this.boxX = this.x - this.boxWidth / 4;     //float; the x location of the box within the canvas.
+
+        this.boxY = this.y - this.boxWidth / 2.5;   //float; the y location of the box within the canvas.
+    }
+
+    //check method will check for hit detection if mouse location is within button area returns true.
+    check() {
+        if (mouseX < this.boxX + this.boxWidth && mouseX > this.boxX && mouseY < this.boxY + this.boxHeight && mouseY > this.boxY) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    //show method displays the target onto the screen.
+    show() {
+        fill(50);
+        if (this.box) { rect(this.boxX, this.boxY, this.boxWidth, this.boxHeight) }
+        fill(255);
+        textSize(this.theSize);
+        text(this.text, this.x, this.y);
+    }
+}
---
+++ wwwroot/AimTrainerGame/GameFunctions.js
@@ -0,0 +1,37 @@
+﻿/*
+ * Author: Jonathan Reardon.
+ * Project: AimTrainerGame.
+ *
+ * Summary: this project is heavily inspire by a flash based game "http://www.aimbooster.com/", all visual aspects are being created using the p5.js library "https://p5js.org/".
+ */
+
+
+/* ::intToTime function::
+ * this function take in an integer (seconds) and converts it into a string in the format (hours : minutes : seconds) 00:00:00
+ * e.g. 120 seconds would be 00:02:00
+ * e.g. 130 seconds would be 00:02:10
+ */
+function intToTime(seconds) {
+    var hours = Math.floor(seconds / 3600);
+    var minutes = Math.floor((seconds - (hours * 3600)) / 60);
+    var seconds = seconds - (hours * 3600) - (minutes * 60);
+
+    if (hours < 10) { hours = "0" + hours; }
+    if (minutes < 10) { minutes = "0" + minutes; }
+    if (seconds < 10) { seconds = "0" + seconds; }
+    return hours + ':' + minutes + ':' + seconds;
+}
+
+
+/* ::heart function::
+ * this function will create a red heart at int x, int y location of size int size;
+ */
+function heart(x, y, size) {
+    fill(255, 50, 50);
+    beginShape();
+    vertex(x, y);
+    bezierVertex(x - size / 2, y - size / 2, x - size, y + size / 3, x, y + size);
+    bezierVertex(x + size, y + size / 3, x + size / 2, y - size / 2, x, y);
+    endShape(CLOSE);
+    fill(255);
+}
