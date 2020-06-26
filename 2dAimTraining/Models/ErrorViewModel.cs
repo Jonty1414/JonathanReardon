fef91539e8d821bdfcafd69cb85a7662ac88f918
Jonathan Reardon <jonty1414@gmail.com>
2020-06-26 14:57:14 +1200

    initial

---
+++ 2dAimTraining/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _2dAimTraining.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
