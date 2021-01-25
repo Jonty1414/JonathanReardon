98b72c20bf0fc3e874320f0d9f6f63a448e24fa2
Jonathan Reardon <jonty1414@gmail.com>
2021-01-25 14:20:18 +1300

    added cv page

--- JonathanReardon/Controllers/HomeController.cs
+++ JonathanReardon/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace JonathanReardon.Controllers
             return View();
         }
 
+        public IActionResult CV()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
